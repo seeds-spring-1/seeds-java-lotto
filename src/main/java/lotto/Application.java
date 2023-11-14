24a4b0e882c3e5cd67fced0aaeba52073f0265f3
10cheon00 <10cheon00@gmail.com>
2023-11-14 17:30:22 +0900

    fix: modify application logic by print sequence

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,23 +3,27 @@ package lotto;
 import java.util.List;
 
 public class Application {
+    private static List<Lotto> PlayerLottoList;
+
     public static void main(String[] args) {
         try {
-            Lotto winningLotto = null;
-
-            // get input
-            Input.run();
-            winningLotto = new Lotto(Input.getWinningLottoNumber());
-
             // generate player's lotto game by input
+            Input.InputPlayerCash();
             LottoGenerator lottoGenerator = new LottoGenerator();
-            List<Lotto> playerLottoList = lottoGenerator.generateRandomLotto(Input.getPlayerCash());
+            Application.PlayerLottoList = lottoGenerator.generateRandomLotto(Input.getPlayerCash());
+
+            // print player lotto list
+            Application.PrintPlayerLottoList();
+
+            // input winning lotto
+            Input.inputWinningLottoNumber();
+            Lotto winningLotto = new Lotto(Input.getWinningLottoNumber());
 
             // decide award
             LottoAwardDecider lottoAwardDecider = new LottoAwardDecider();
             LottoAward lottoAward = lottoAwardDecider.decideAward(
                     winningLotto,
-                    playerLottoList
+                    Application.PlayerLottoList
             );
 
             // print award and profit rate
@@ -27,6 +31,12 @@ public class Application {
         }
         catch(IllegalArgumentException exception) {
             System.out.println("[ERROR] 로또 번호는 1부터 45사이의 숫자여야 합니다.");
+        }
+    }
+
+    public static void PrintPlayerLottoList() {
+        for(Lotto lotto : Application.PlayerLottoList) {
+            System.out.println(lotto);
         }
     }
 }
