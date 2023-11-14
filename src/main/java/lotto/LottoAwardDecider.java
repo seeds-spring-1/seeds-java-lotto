5aa90091b70e0139cf6e14433f61ab19a99310cc
10cheon00 <10cheon00@gmail.com>
2023-11-14 18:32:14 +0900

    feat: create LottoAwardDecider and LottoScore classes

--- src/main/java/lotto/LottoAwardDecider.java
+++ src/main/java/lotto/LottoAwardDecider.java
@@ -1,2 +1,47 @@
-package lotto;public class LottoAwardDecider {
+package lotto;
+
+import java.util.List;
+
+public class LottoAwardDecider {
+    private LottoScore lottoScore;
+
+    public LottoAwardDecider() {
+    }
+
+    public LottoAward decideAward(WinningLotto winningLotto, List<Lotto> playerLottoList) {
+        LottoAward lottoAward = new LottoAward();
+
+        for (Lotto playerLotto : playerLottoList) {
+            // get count of equal number from winningLotto
+            checkPlayerLottoScoreByWinningLotto(playerLotto, winningLotto);
+
+            // if count is 5 and there is number which is bonus number,
+            // award 2nd rank.
+            lottoAward.updateAward(lottoScore);
+        }
+        return lottoAward;
+    }
+
+    public void checkPlayerLottoScoreByWinningLotto(Lotto playerLotto, WinningLotto winningLotto) {
+        lottoScore = new LottoScore();
+
+        for(Integer number : playerLotto.get()) {
+            boolean isEqualNumberExist = false;
+
+            for(Integer winningNumber : winningLotto.get()) {
+                if (number.equals(winningNumber)) {
+                    isEqualNumberExist = true;
+                    break;
+                }
+            }
+
+            if(isEqualNumberExist) {
+                lottoScore.increaseScore();
+            }
+
+            if (number.equals(winningLotto.getSpecialNumber())){
+                lottoScore.earnBonus();
+            }
+        }
+    }
 }
--- src/main/java/lotto/LottoScore.java
+++ src/main/java/lotto/LottoScore.java
@@ -1,2 +1,27 @@
-package lotto;public class LottoScore {
+package lotto;
+
+public class LottoScore {
+    private int score;
+    private boolean bonus;
+
+    public LottoScore() {
+        score = 0;
+        bonus = false;
+    }
+
+    public void increaseScore() {
+        score++;
+    }
+
+    public void earnBonus() {
+        bonus = true;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public boolean isEarnBonus() {
+        return bonus;
+    }
 }
