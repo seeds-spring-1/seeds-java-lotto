2b0345ec52cfb75428d359b62cee0467c32013f4
10cheon00 <10cheon00@gmail.com>
2023-11-14 17:48:06 +0900

    feat: create LottoGenerator

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -1,2 +1,28 @@
-package lotto;public class LottoGenerator {
+package lotto;
+
+
+import java.util.List;
+import java.util.Vector;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class LottoGenerator {
+    public LottoGenerator() {
+    }
+
+    public List<Lotto> generateRandomLottoListByCash(int cash) {
+        Vector<Lotto> lottoList = new Vector<>();
+
+        cash /= 1000;
+
+        while (cash-- > 0) {
+            lottoList.add(this.generateRandomLotto());
+        }
+
+        return lottoList;
+    }
+
+    private Lotto generateRandomLotto() {
+        return new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+    }
 }
