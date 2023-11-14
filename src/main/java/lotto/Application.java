51c2aa15a550e172789ce0d7777a8e4086c8218c
10cheon00 <10cheon00@gmail.com>
2023-11-14 17:09:17 +0900

    feat: create main logic

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,31 @@
 package lotto;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Lotto winningLotto = null;
+        try {
+            // get input
+            Input.run();
+            winningLotto = new Lotto(Input.getWinningLottoNumber());
+
+            // generate player's lotto game by input
+            LottoGenerator lottoGenerator = new LottoGenerator();
+            List<Lotto> playerLottoList = lottoGenerator.generateRandomLotto(Input.getPlayerCash());
+
+            // decide award
+            LottoAwardDecider lottoAwardDecider = new LottoAwardDecider();
+            LottoAward lottoAward = lottoAwardDecider.decideAward(
+                    winningLotto,
+                    playerLottoList
+            );
+
+            // print award and profit rate
+            LottoAwardPresenter.printAwardAndProfitRate(lottoAward);
+        }
+        catch(IllegalArgumentException exception) {
+            System.out.println("[ERROR] 로또 번호는 1부터 45사이의 숫자여야 합니다.");
+        }
     }
 }
