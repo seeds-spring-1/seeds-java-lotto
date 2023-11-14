9f46ea3a62e6be72ff2b8247e6520571a5626134
10cheon00 <10cheon00@gmail.com>
2023-11-15 00:42:04 +0900

    feat: implement LottoAward class

--- src/main/java/lotto/LottoAward.java
+++ src/main/java/lotto/LottoAward.java
@@ -1,2 +1,43 @@
-package lotto;public class LottoAward {
+package lotto;
+
+import java.util.HashMap;
+
+public class LottoAward {
+    private final HashMap<Award, Integer> awardHashMap;
+
+    public LottoAward() {
+        awardHashMap = new HashMap<>();
+        for (Award award : Award.values()) {
+            awardHashMap.put(award, 0);
+        }
+    }
+
+    public void updateAward(LottoScore lottoScore) {
+        Award award = getAwardByScore(lottoScore);
+        int count = awardHashMap.get(award);
+        awardHashMap.replace(award, count + 1);
+    }
+
+    private Award getAwardByScore(LottoScore lottoScore) {
+        int score = lottoScore.getScore();
+
+        // TODO: 단순 if로 나열한 비즈니스 로직 해결해보기
+        if (score == 6) {
+            return Award.Score6;
+        }
+        if (score == 5 && lottoScore.hasBonus()) {
+            return Award.Score5AndBonus;
+        }
+        if (score == 5) {
+            return Award.Score5;
+        }
+        if (score == 4) {
+            return Award.Score4;
+        }
+        return Award.Score3;
+    }
+
+    public HashMap<Award, Integer> getAward() {
+        return new HashMap<>(this.awardHashMap);
+    }
 }
