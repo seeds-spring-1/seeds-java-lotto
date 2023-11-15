7884f18209a04b581bbe2e986364892ce2e21f6b
10cheon00 <10cheon00@gmail.com>
2023-11-15 12:29:07 +0900

    fix: change lotto award data structure
    change from HashMap to ArrayList.
    HashMap doesn't provide order of items.

--- src/main/java/lotto/LottoAwardPresenter.java
+++ src/main/java/lotto/LottoAwardPresenter.java
@@ -2,17 +2,24 @@ package lotto;
 
 import org.assertj.core.groups.Tuple;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class LottoAwardPresenter {
     private class AwardTuple {
+        private final Award award;
         private final Integer reward;
         private final String presentSting;
 
-        public AwardTuple(Integer reward, String presentSting) {
+        public AwardTuple(Award award, Integer reward, String presentSting) {
+            this.award = award;
             this.reward = reward;
             this.presentSting = presentSting;
+        }
+
+        public Award getAward() {
+            return award;
         }
 
         public Integer getReward() {
@@ -24,28 +31,26 @@ public class LottoAwardPresenter {
         }
     }
 
-    private final HashMap<Award, AwardTuple> awardStringHashMap;
+    private final ArrayList<AwardTuple> awardTupleList;
 
     public LottoAwardPresenter() {
-        awardStringHashMap = new HashMap<>();
+        awardTupleList = new ArrayList<>();
         createAwardString();
     }
 
     private void createAwardString() {
-        awardStringHashMap.put(Award.Score3, new AwardTuple(5000, "3개 일치 (5,000원)"));
-        awardStringHashMap.put(Award.Score4, new AwardTuple(50000, "4개 일치 (50,000원)"));
-        awardStringHashMap.put(Award.Score5, new AwardTuple(1500000, "5개 일치 (1,500,000원)"));
-        awardStringHashMap.put(Award.Score5AndBonus, new AwardTuple(30000000, "5개 일치, 보너스 볼 일치 (30,000,000원)"));
-        awardStringHashMap.put(Award.Score6, new AwardTuple(2000000000, "6개 일치 (2,000,000,000원)"));
+        awardTupleList.add(new AwardTuple(Award.Score3, 5000, "3개 일치 (5,000원)"));
+        awardTupleList.add(new AwardTuple(Award.Score4, 50000, "4개 일치 (50,000원)"));
+        awardTupleList.add(new AwardTuple(Award.Score5, 1500000, "5개 일치 (1,500,000원)"));
+        awardTupleList.add(new AwardTuple(Award.Score5AndBonus, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원)"));
+        awardTupleList.add(new AwardTuple(Award.Score6, 2000000000, "6개 일치 (2,000,000,000원)"));
     }
 
     public void printAwardAndProfitRate(LottoAward lottoAward) {
         long reward = 0;
 
-        for (Map.Entry<Award, AwardTuple> entry : awardStringHashMap.entrySet()) {
-            Award award = entry.getKey();
-            AwardTuple tuple = entry.getValue();
-            System.out.printf("%s - %d개\n", tuple.getPresentString(), lottoAward.getAwardCount(award));
+        for (AwardTuple tuple : awardTupleList) {
+            System.out.printf("%s - %d개\n", tuple.getPresentString(), lottoAward.getAwardCount(tuple.getAward()));
             reward += tuple.getReward();
         }
 
